c058ed9fd3ebfbd55506b0d433f10f778c16551d
nowakpawel <kontakt.nowakpawel@gmail.com>
2025-09-28 21:10:44 +0200

    Update About

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1758835196',resources: [
+    cacheName: 'chirpy-1759086535',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
